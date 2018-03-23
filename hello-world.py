80643274d100044c590cd5c88e785df2a1067ad5
ausmith <goldsmithaaron@gmail.com>
2018-03-23 13:48:27 -0400

    Add python things

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    print("Hello {}!".format(args.name))
